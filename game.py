bbe68bee0027a5ff68623de0cda1a9c7884b9660
KScholze <118296097+KScholze@users.noreply.github.com>
2022-12-18 17:05:30 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,12 @@
+
+from text_game.game_logic import *
+
+label_entry.configure(
+    text =
+    "XXX Willkommen im Dungeon XXX\n"
+    "Du bist in einem dunklen Verließ und musst einen Schatz suchen\n"
+    "Doch du bist nicht alleine. Sei auf der Hut...\n"
+)
+
+# mainloop erst hier starten!
+fenster.mainloop()
